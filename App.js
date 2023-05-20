dbd922ae759623ac657066365e0400b9454d4b0d
KamykOOO <4m4dik@gmail.com>
2023-05-20 15:54:57 +0200

    add input to add task with styles

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import Task from './components/Task'
 export default function App() {
+  const handleTask = () => { }
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
@@ -13,7 +14,14 @@ export default function App() {
           <Task text="Task 3"></Task>
         </View>
       </View>
-      <View style={styles.items}></View>
+      <KeyboardAvoidingView style={styles.writeTaskWrapper}>
+        <TextInput style={styles.unput} placeholder={'Write a task'}></TextInput>
+        <TouchableOpacity onPress={() => handleTask}>
+          <View style={styles.addWraper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -36,5 +44,35 @@ const styles = StyleSheet.create({
 
   items: {
     marginTop: 30,
-  }
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWraper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+  },
+  addText: {
+
+  },
 });
