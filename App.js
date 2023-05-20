24a615b70ec836f629c0c382d6b9334769ed14bc
KamykOOO <4m4dik@gmail.com>
2023-05-20 14:22:00 +0200

    Add basic structure

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>
+          Tytuł taksa!
+        </Text>
+      </View>
+      <View style={styles.items}></View>
     </View>
   );
 }
@@ -13,8 +16,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#n8eaed',
   },
+
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  items: {
+    marginTop: 30,
+  }
 });
